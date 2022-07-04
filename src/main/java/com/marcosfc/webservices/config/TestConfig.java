0e0f6c2321154d4b0404d11d1f8e6c762e94b02e
Marcos Fernandes <marcosfc.ads@gmail.com>
2022-07-04 08:23:49 -0300

    Payment, one-to-one association

--- src/main/java/com/marcosfc/webservices/config/TestConfig.java
+++ src/main/java/com/marcosfc/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.marcosfc.webservices.entities.Category;
 import com.marcosfc.webservices.entities.Order;
 import com.marcosfc.webservices.entities.OrderItem;
+import com.marcosfc.webservices.entities.Payment;
 import com.marcosfc.webservices.entities.Product;
 import com.marcosfc.webservices.entities.User;
 import com.marcosfc.webservices.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
